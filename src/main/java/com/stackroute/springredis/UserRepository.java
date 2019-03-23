881844634b49a2439605a4a9d3b4b53549868dca
roopali <roopaligandhi900@gmail.com>
2019-03-23 17:54:13 +0530

    task

---
+++ src/main/java/com/stackroute/springredis/UserRepository.java
@@ -0,0 +1,14 @@
+package com.stackroute.springredis;
+
+import com.stackroute.springredis.model.User;
+
+import java.util.Map;
+
+public interface UserRepository {
+
+    void save(User user);
+    Map<String, User> findAll();
+    User findById(String id);
+    void update(User user);
+    void delete(String id);
+}
